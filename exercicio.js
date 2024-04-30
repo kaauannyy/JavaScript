d5ac37636a4d4bb1bb5e3935796255721b1a4e4e
Kauanny Fernandes <kauanny.fernandes.0803@gmail.com>
2024-04-30 17:32:26 -0300

    Aprendemos o javascript, as variaveis e prompt.

---
+++ exercicio.js
@@ -0,0 +1,7 @@
+
+let numero1 = parseInt(prompt("Insira um número:"));
+let numero2 = parseInt(prompt("Insira um número:"));
+
+soma = numero1 + numero2;
+
+alert(`O resultado da some é ${soma}`);
---
+++ exercicio5.js
@@ -0,0 +1,4 @@
+let nome = prompt("Qual é o seu nome?");
+let idade = prompt("Qual é  sua idade?");
+let bairro = prompt("Qual é o bairro que você mora?")
+
