91ebfffc0cbe7ab87908215269b9cb5e5f6d4b8b
Kauanny Fernandes <kauanny.fernandes.0803@gmail.com>
2024-05-02 16:37:36 -0300

    aprendemos algumas funções especificas para string, como lenght, split, entre outros

--- exercicio.js
+++
@@ -1,7 +0,0 @@
-
-let numero1 = parseInt(prompt("Insira um número:"));
-let numero2 = parseInt(prompt("Insira um número:"));
-
-soma = numero1 + numero2;
-
-alert(`O resultado da some é ${soma}`);
--- exercicio5.js
+++
@@ -1,4 +0,0 @@
-let nome = prompt("Qual é o seu nome?");
-let idade = prompt("Qual é  sua idade?");
-let bairro = prompt("Qual é o bairro que você mora?")
-
